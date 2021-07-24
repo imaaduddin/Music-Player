b1cba5696e03cfb21105391b60ace7594de1974c
Imaad Uddin <imaaduddin@icloud.com>
2021-07-23 19:12:25 -0500

    functionality

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+  {
+    name: "",
+    displayName: "",
+    artist: ""
+  }
+];
 
 // Check if playing
 let isPlaying = false;
@@ -9,12 +21,16 @@ let isPlaying = false;
 // Play 
 function playSong() {
   isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
   music.play();
 }
 
 // Pause 
 function pauseSong() {
   isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
   music.pause();
 }
 
