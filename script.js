31c4d1593078996fb48f53885258a3ad18a714d0
Imaad Uddin <imaaduddin@icloud.com>
2021-07-25 17:00:43 -0500

    working on progress bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -55,5 +57,41 @@ function loadSong(song) {
   image.src = `Images/${song.name}.jpg`;
 }
 
+// Current Song
+let songIndex = 0;
+
+// Prev Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
 // On Load - Select First Song 
-loadSong(songs[1]);
+loadSong(songs[songIndex]);
+
+// Update Progress Bad & Time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const {duration, currentTime} = e.srcElement;
+    console.log(duration, currentTime);
+  }
+}
+
+// Event Listeners 
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
